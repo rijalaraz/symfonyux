2c3fa466f167fef69454f5872f9d1eb84987ad78
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-04-11 16:20:13 +0300

    Pour éviter de cliquer deux fois

--- src/Twig/Components/Post/PostListComponent.php
+++ src/Twig/Components/Post/PostListComponent.php
@@ -5,18 +5,22 @@ namespace App\Twig\Components\Post;
 use App\Repository\PostRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
 
 #[AsLiveComponent]
 final class PostListComponent
 {
     use DefaultActionTrait;
 
+    public ?array $posts  = [];
+
     public function __construct(
         private PostRepository $postRepository
     ) {}
 
-    public function getPosts(): array
+    #[PreMount()]
+    public function getAllPosts(): void
     {
-        return $this->postRepository->findAll();
+        $this->posts = $this->postRepository->findAll();
     }
 }
