7e36f3fb73f7355370c60fbaee5186e8a3a11c46
Rija Lalaina <rijalaraz@gmail.com>
2025-05-08 22:21:04 +0300

    Styling of Chart.js

--- src/Twig/Components/Chart/ChartComponent.php
+++ src/Twig/Components/Chart/ChartComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig\Components\Chart;
 
+use App\Service\ChartService;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
@@ -16,47 +17,59 @@ final class ChartComponent
 
     public function __construct(
         private ChartBuilderInterface $chartBuilder,
+        private ChartService $chartService,
     ) {
     }
 
     #[LiveProp(writable: true)]
-    public int $echelle = 0;
+    public int $echelle = 50;
 
     #[ExposeInTemplate]
     public function getChart()
     {
         $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
 
-        $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            'datasets' => [
-                [
-                    'label' => 'Cookies eaten 🍪',
-                    'backgroundColor' => 'rgb(255, 99, 132, .4)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [2, 10, 5, 18, 20, 30, 45],
-                    'fill' => true,
-                    'tension' => 0.1,
-                ],
-                [
-                    'label' => 'Km walked 🏃‍♀️',
-                    'backgroundColor' => 'rgba(45, 220, 126, .4)',
-                    'borderColor' => 'rgba(45, 220, 126)',
-                    'data' => [10, 15, 4, 3, 25, 41, 25],
-                    'tension' => 0.4,
-                ],
-            ],
-        ]);
+        $chart->setData($this->chartService->fetchData(
+            $this->echelle
+        ));
 
         $chart->setOptions([
             'maintainAspectRatio' => false,
             'scales' => [
+                'x' => [
+                    'display' => true,
+                    'title' => [
+                        'display' => true,
+                        'text' => 'Month',
+                        'color' => '#911',
+                        'font' => [
+                            'family' => 'Comic Sans MS',
+                            'size' => 20,
+                            'weight' => 'bold',
+                            'lineHeight' => 1.2,
+                        ],
+                        'padding' => ['top' => 20, 'left' => 0, 'right' => 0, 'bottom' => 0]
+                    ]
+                ],
                 'y' => [
+                    'display' => true,
                     'type' => 'logarithmic',
                     'position' => 'left',
                     'min' => 0,
                     'max' => 100,
-                ],
+                    'title' => [
+                        'display' => true,
+                        'text' => 'Value',
+                        'color' => '#191',
+                        'font' => [
+                            'family' => 'Times',
+                            'size' => 20,
+                            'style' => 'normal',
+                            'lineHeight' => 1.2
+                        ],
+                        'padding' => ['top' => 30, 'left' => 0, 'right' => 0, 'bottom' => 0]
+                    ]
+                ]
             ],
             'plugins' => [
                 'zoom' => [
@@ -65,6 +78,38 @@ final class ChartComponent
                         'pinch' => ['enabled' => true],
                         'mode' => 'xy',
                     ],
+                ],
+                'title' => [
+                    'display' => true,
+                    'text' => \sprintf(
+                        'Echelle de %d%%',
+                        abs($this->echelle)
+                    ),
+                    'color' => '#119',
+                    'font' => [
+                        'family' => 'Arial',
+                        'size' => 22,
+                        'style' => 'normal',
+                        'lineHeight' => 1.2
+                    ],
+                ],
+                'legend' => [
+                    'labels' => [
+                        'boxHeight' => 20,
+                        'boxWidth' => 50,
+                        'padding' => 20,
+                        'font' => [
+                            'size' => 14,
+                        ],
+                    ],
+                ],
+            ],
+            'elements' => [
+                'line' => [
+                    'borderWidth' => 5,
+                    'tension' => 0.25,
+                    'borderCapStyle' => 'round',
+                    'borderJoinStyle' => 'round',
                 ],
             ],
         ]);
