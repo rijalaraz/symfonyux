e467122e8cba227eb846411deb0c7955e483afc1
Rija Lalaina <rijalaraz@gmail.com>
2025-05-13 23:23:11 +0300

    Modal Photo suite

---
+++ src/Twig/Components/ModalComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\Photo;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class ModalComponent
+{
+    use DefaultActionTrait;
+
+    #[LiveProp()]
+    public ?string $id = null;
+
+    #[LiveProp()]
+    public ?Photo $photo = null;
+}
