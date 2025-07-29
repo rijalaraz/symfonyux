787e6b90ebf24e1947a9f5bc002ea4cd69924842
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-07-29 16:12:04 +0300

    Unused code

--- src/Twig/Components/Blog/EditBlogComponent.php
+++ src/Twig/Components/Blog/EditBlogComponent.php
@@ -6,8 +6,6 @@ use App\Entity\Blog;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
-use Symfony\UX\LiveComponent\Attribute\LiveArg;
-use Symfony\UX\LiveComponent\Attribute\LiveListener;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\ValidatableComponentTrait;
@@ -37,11 +35,5 @@ final class EditBlogComponent
 
         $this->is_edited = true;
         $this->em->flush();
-    }
-
-    #[LiveListener('titleChanged')]
-    public function createSlug(#[LiveArg] string $slug)
-    {
-        $this->blog->setContent($slug);
     }
 }
